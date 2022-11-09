7f798ebda81f44348dbbe67127f588ce57be8c0a
Hal Motley <inferno986returns@gmail.com>
2022-11-09 16:49:16 +0000

    ebookbuild.py-3.2 is actually generating files now
    The meat is getting the content.opf working and connecting it to the metadata.json

--- README.md
+++ README.md
@@ -3,17 +3,26 @@ An unofficial e-book for Skyrim's vast array of texts. Happy 11<sup>th</sup> ann
 
 This project's goal is to provide a complete and beautiful reading experience for Skyrim's impressive selection of books in one anthology.
 
-The goal isn't to present the books verbatim as we have wikis for that. Instead to reformat the books, correcting any typos and using standardised punctuation.
+The goal isn't to present the books verbatim as we have wikis for that. Instead, the aim is to reformat the books, correcting any typos and using standardised punctuation to provide an enjoyable reading experience on any EPUB3-compatible device.
 
 In addition, this will be my chance to try making a fully standards-compliant EPUB 3.2 book!
 
-## Licencing
-* Each of the book's text and images are copyright ZeniMax Media and Bethesda Softworks. The content is re-used in a transformatively with no profit incentive in a similar manner to a Skyrim mod.
+## Compiling
+The following components are required:
+* Python 3
+* Bash (zsh may be just fine on macOS)
+* OpenJDK
+* Windows Subsystem for Linux (WSL) (on Microsoft Windows)
 
-* ebookbuild's Python source code is licenced under the GNU General Public License v3 (GPLv3).
+The new `ebookbuild` uses the popular lxml Python library which can be installed using `pip install lxml` in the Bash terminal.
+
+## Licencing
+* Each of the book's text and images is copyrighted to ZeniMax Media and Bethesda Softworks. The content is re-used in a transformative manner with no profit incentive similarly to a Skyrim mod.
+
+* `ebookbuild`'s Python source code is licenced under the GNU General Public License v3 (GPLv3).
 
 * The EPUB's XHTML, CSS and JSON source code are licenced under Creative Commons Zero 1.0 (CC0).
 
 * The Oblivion font is by Steve Deffeyes (aka dongle) who has released it under the terms of a freeware licence.
 
-* Epubcheck 4.26 is licenced under the Apache License 2.0. Other related licences are in folder in the e-book directory.
+* Epubcheck 4.26 is licenced under the Apache License 2.0. Other related licences are in the "licenses" folder in the e-book directory.
---
+++ e-book/ebookbuild-3.2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env
+
+# ebookbuild-3.2.py - ebookbuild for the EPUB3 standard. Licenced under GNU GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+from lxml import etree
+import os
+
+#Test variable which I'll connect to metadata.json in the final release.
+rootfolder = "e-book"
+containerfolder = "OPS"
+opfname = "content.opf"
+
+def mimetype():
+    #Creates the simple mimetype file
+    if os.path.isfile(rootfolder + os.sep + "mimetype") == False:
+        os.chdir(rootfolder)
+        with open("mimetype", "w") as mt:
+            mt.write("application/epub+zip")
+        print("Created the mimetype file.")
+
+    else: 
+        print("Mimetype file already exists.")
+
+def metainf_folder():
+     #Create the META-INF folder
+    if os.path.isdir(rootfolder + os.sep + "META-INF") == False:
+        os.chdir(rootfolder)
+        os.mkdir("META-INF")
+    else:
+        print("META-INF folder already exists.")
+        
+def containerxml():
+    # Create the EPUB3 container.xml file
+    if os.path.exists(rootfolder + os.sep + "META-INF" + os.sep + "container.xml") == False:
+        os.chdir(rootfolder + os.sep + "META-INF")
+        root = etree.Element("container")
+        root.set("xmlns", "urn:oasis:names:tc:opendocument:xmlns:container")
+        root.set("version", "1.0")
+
+        rootfiles = etree.SubElement(root, "rootfiles")
+
+        rootfile = etree.SubElement(rootfiles, "rootfile")
+        rootfile.set("full-path", "OPS/package.opf")
+        rootfile.set("media-type", "application/oebps-package+xml")
+
+        etree.ElementTree(root).write("container.xml", encoding="utf-8", xml_declaration=True, pretty_print=True)
+        print("The META-INF/container.xml has been created.")
+
+    else:
+        print("META-INF folder already exists.")
+              
+def opf():
+# Generate the EPUB3 .opf file
+        os.chdir(rootfolder + os.sep + containerfolder)
+        root = etree.Element("container")
+        root.set("xmlns", "urn:oasis:names:tc:opendocument:xmlns:container")
+        root.set("version", "1.0")
+
+        rootfiles = etree.SubElement(root, "rootfiles")
+
+        rootfile = etree.SubElement(rootfiles, "rootfile")
+        rootfile.set("full-path", "OPS/package.opf")
+        rootfile.set("media-type", "application/oebps-package+xml")
+
+        etree.ElementTree(root).write(opfname, encoding="utf-8", xml_declaration=True, pretty_print=True)
+        print("The " + containerfolder + "/" + opfname + " has been created.")
+
+mimetype()
+metainf_folder()
+containerxml()
+opf()
